e4b8c797d49facda7d88d12252bc6a38b9ef144a
Francisco  Inoque <40237204+frantchessico@users.noreply.github.com>
2019-12-15 17:19:21 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 const fireConfig = {
-    apiKey: "AIzaSyDrZH3Oou9-p3jNomEG6Dgjm2ExONgQang",
-    authDomain: "naminhaloja.firebaseapp.com",
-    databaseURL: "https://naminhaloja.firebaseio.com",
-    projectId: "naminhaloja",
-    storageBucket: "naminhaloja.appspot.com",
-    messagingSenderId: "663817652034",
-    appId: "1:663817652034:web:1f5c86eeddb3aa96"
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#"
   };
   
   firebase.initializeApp(fireConfig);
